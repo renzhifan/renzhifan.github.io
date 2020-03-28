42fca3bb7437ad4bd54a3b8a51a519df0981445a
renzhifan <zhifan1005@163.com>
2020-03-28 21:12:56 +0800

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 /*!--------------------------------*\
    3-Jekyll Theme
    @author Peiwen Lu (P233)
@@ -6,136 +5,160 @@
 \*---------------------------------*/
 
 // cnzz
-var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cspan id='cnzz_stat_icon_1256860805'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1256860805%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
-
+var cnzz_protocol = 'https:' == document.location.protocol ? ' https://' : ' http://'
+document.write(unescape("%3Cspan id='cnzz_stat_icon_1278714341'%3E%3C/span%3E%3Cscript src='https://s9.cnzz.com/z_stat.php%3Fid%3D1278714341%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"))
 // Variables
-var sidebar    = $('#sidebar'),
-    container  = $('#post'),
-    content    = $('#pjax'),
-    button     = $('#icon-arrow');
-    scrollTop  = $('#icon-arrow-up');
-    tocbar     = $('#post__toc-trigger');
+var sidebar = $('#sidebar'),
+  container = $('#post'),
+  content = $('#pjax'),
+  button = $('#icon-arrow')
+scrollTop = $('#icon-arrow-up')
+tocbar = $('#post__toc-trigger')
 
 // Detect window size, if less than 1280px add class 'mobile' to sidebar therefore it will be auto hide when trigger the pjax request in small screen devices.
 if ($(window).width() < 1280) {
-  sidebar.addClass('mobile');
+  sidebar.addClass('mobile')
 }
 
 // Tags switcher
 var clickHandler = function(id) {
   return function() {
-    $(this).addClass('active').siblings().removeClass('active');
-    $('.pl__all').hide();
-    $('.' + id).delay(50).fadeIn(350);
+    $(this)
+      .addClass('active')
+      .siblings()
+      .removeClass('active')
+    $('.pl__all').hide()
+    $('.' + id)
+      .delay(50)
+      .fadeIn(350)
   }
-};
+}
 
-$('#tags__ul li').each(function(index){
-  $('#' + $(this).attr('id')).on('click', clickHandler($(this).attr('id')));
-});
+$('#tags__ul li').each(function(index) {
+  $('#' + $(this).attr('id')).on('click', clickHandler($(this).attr('id')))
+})
 
 // If sidebar has class 'mobile', hide it after clicking.
 $('.pl__all').on('click', function() {
-  $(this).addClass('active').siblings().removeClass('active');
+  $(this)
+    .addClass('active')
+    .siblings()
+    .removeClass('active')
   if (sidebar.hasClass('mobile')) {
-    $('#sidebar, #pjax, #icon-arrow').addClass('fullscreen');
+    $('#sidebar, #pjax, #icon-arrow').addClass('fullscreen')
   }
-});
+})
 
 // Enable fullscreen.
 $('#js-fullscreen').on('click', function() {
   if (button.hasClass('fullscreen')) {
-    sidebar.removeClass('fullscreen');
-    button.removeClass('fullscreen');
-    tocbar.removeClass('fullscreen');
-    content.delay(300).queue(function(){
-      $(this).removeClass('fullscreen').dequeue();
-    });
+    sidebar.removeClass('fullscreen')
+    button.removeClass('fullscreen')
+    tocbar.removeClass('fullscreen')
+    content.delay(300).queue(function() {
+      $(this)
+        .removeClass('fullscreen')
+        .dequeue()
+    })
   } else {
-    sidebar.addClass('fullscreen');
-    button.addClass('fullscreen');
-    tocbar.addClass('fullscreen');
-    content.delay(200).queue(function(){
-      $(this).addClass('fullscreen').dequeue();
-    });
+    sidebar.addClass('fullscreen')
+    button.addClass('fullscreen')
+    tocbar.addClass('fullscreen')
+    content.delay(200).queue(function() {
+      $(this)
+        .addClass('fullscreen')
+        .dequeue()
+    })
   }
-});
+})
 
 $('#scroll-top').on('click', function() {
-  container.animate({
-    scrollTop: 0
-  }, 200);
-});
+  container.animate(
+    {
+      scrollTop: 0
+    },
+    200
+  )
+})
 
-$('#mobile-avatar').on('click', function(){
-  $('#sidebar, #pjax, #icon-arrow').addClass('fullscreen');
-});
+$('#mobile-avatar').on('click', function() {
+  $('#sidebar, #pjax, #icon-arrow').addClass('fullscreen')
+})
 
 // Pjax
-$(document).pjax('#avatar, #mobile-avatar, .pl__all', '#pjax', { fragment: '#pjax', timeout: 10000 });
+$(document).pjax('#avatar, #mobile-avatar, .pl__all', '#pjax', { fragment: '#pjax', timeout: 10000 })
 $(document).on({
   'pjax:click': function() {
-    content.removeClass('fadeIn').addClass('fadeOut');
-    NProgress.start();
+    content.removeClass('fadeIn').addClass('fadeOut')
+    NProgress.start()
   },
   'pjax:start': function() {
-    content.css({'opacity':0});
+    content.css({ opacity: 0 })
   },
   'pjax:end': function() {
-    NProgress.done();
-    container.scrollTop(0);
-    content.css({'opacity':1}).removeClass('fadeOut').addClass('fadeIn');
-    afterPjax();
+    NProgress.done()
+    container.scrollTop(0)
+    content
+      .css({ opacity: 1 })
+      .removeClass('fadeOut')
+      .addClass('fadeIn')
+    afterPjax()
   }
-});
+})
 
 // Re-run scripts for post content after pjax
 function afterPjax() {
   // Open links in new tab
-  $('#post__content a').attr('target','_blank');
+  $('#post__content a').attr('target', '_blank')
 
   // Generate post TOC for h1 and h2
-  var toc = $('#post__toc-ul');
+  var toc = $('#post__toc-ul')
   // Empty TOC and generate an entry for post title
   var title = $('#post__title').text()
-  toc.empty().append('<li class="post__toc-li post__toc-h1"><a href="#post__title" class="js-anchor-link">' + title + '</a></li>');
+  toc.empty().append('<li class="post__toc-li post__toc-h1"><a href="#post__title" class="js-anchor-link">' + title + '</a></li>')
 
   // Generate entries for h1 and h2
-  $('#post__content').children('h1,h2,h3').each(function() {
-    // Skip post title
-    if ($(this).attr('id') == 'post__title') {
-      return;
-    }
-
-    // Generate random ID for each heading
-    $(this).attr('id', function() {
-      var ID = "",
-          alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-      for(var i=0; i < 5; i++) {
-        ID += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+  $('#post__content')
+    .children('h1,h2,h3')
+    .each(function() {
+      // Skip post title
+      if ($(this).attr('id') == 'post__title') {
+        return
       }
-      return ID;
-    });
 
-    // Add each chapter to TOC
-    if ($(this).prop("tagName") == 'H2') {
-      toc.append('<li class="post__toc-li post__toc-h2"><a href="#' + $(this).attr('id') + '" class="js-anchor-link">' + $(this).text() + '</a></li>');
-    } else if ($(this).prop("tagName") == 'H3') {
-      toc.append('<li class="post__toc-li post__toc-h3"><a href="#' + $(this).attr('id') + '" class="js-anchor-link">' + $(this).text() + '</a></li>');
-    }
-  });
+      // Generate random ID for each heading
+      $(this).attr('id', function() {
+        var ID = '',
+          alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+        for (var i = 0; i < 5; i++) {
+          ID += alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+        }
+        return ID
+      })
+
+      // Add each chapter to TOC
+      if ($(this).prop('tagName') == 'H2') {
+        toc.append('<li class="post__toc-li post__toc-h2"><a href="#' + $(this).attr('id') + '" class="js-anchor-link">' + $(this).text() + '</a></li>')
+      } else if ($(this).prop('tagName') == 'H3') {
+        toc.append('<li class="post__toc-li post__toc-h3"><a href="#' + $(this).attr('id') + '" class="js-anchor-link">' + $(this).text() + '</a></li>')
+      }
+    })
 
   // Smooth scrolling
   $('.js-anchor-link').on('click', function() {
-    var target = $(this.hash);
-    container.animate({scrollTop: target.offset().top + container.scrollTop() - 70}, 500, function() {
-      target.addClass('flash').delay(700).queue(function() {
-        $(this).removeClass('flash').dequeue();
-      });
-    });
-  });
+    var target = $(this.hash)
+    container.animate({ scrollTop: target.offset().top + container.scrollTop() - 70 }, 500, function() {
+      target
+        .addClass('flash')
+        .delay(700)
+        .queue(function() {
+          $(this)
+            .removeClass('flash')
+            .dequeue()
+        })
+    })
+  })
 
   // Lazy Loading Disqus
   // http://jsfiddle.net/dragoncrew/SHGwe/1/
@@ -164,7 +187,7 @@ function afterPjax() {
   // container.scroll(check);
 
   if (sidebar.hasClass('mobile')) {
-    $('#sidebar, #pjax, #icon-arrow').addClass('fullscreen');
+    $('#sidebar, #pjax, #icon-arrow').addClass('fullscreen')
   }
-
-}afterPjax();
+}
+afterPjax()
